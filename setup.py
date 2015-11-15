2467caf5a6bf1452b20d84160c231f7f8be2e62d
Ondřej Nový <ondrej.novy@firma.seznam.cz>
2015-11-15 21:11:36 +0100

    Change setup.py to OS one
    Change-Id: Ia23c12de5f6448793aa3584e3041711d68b92953

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python
-# Copyright (c) 2010-2011 OpenStack, LLC.
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,76 +13,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from setuptools import setup, find_packages
-from setuptools.command.sdist import sdist
-import os
-import subprocess
+# THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
+import setuptools
+
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
 try:
-    from babel.messages import frontend
+    import multiprocessing  # noqa
 except ImportError:
-    frontend = None
+    pass
 
-from swauth import __version__ as version
-
-
-class local_sdist(sdist):
-    """Customized sdist hook - builds the ChangeLog file from VC first"""
-
-    def run(self):
-        if os.path.isdir('.bzr'):
-            # We're in a bzr branch
-
-            log_cmd = subprocess.Popen(["bzr", "log", "--gnu"],
-                                       stdout=subprocess.PIPE)
-            changelog = log_cmd.communicate()[0]
-            with open("ChangeLog", "w") as changelog_file:
-                changelog_file.write(changelog)
-        sdist.run(self)
-
-
-name = 'swauth'
-
-
-cmdclass = {'sdist': local_sdist}
-
-
-if frontend:
-    cmdclass.update({
-        'compile_catalog': frontend.compile_catalog,
-        'extract_messages': frontend.extract_messages,
-        'init_catalog': frontend.init_catalog,
-        'update_catalog': frontend.update_catalog,
-    })
-
-
-setup(
-    name=name,
-    version=version,
-    description='Swauth',
-    license='Apache License (2.0)',
-    author='OpenStack, LLC.',
-    author_email='swauth@brim.net',
-    url='https://github.com/openstack/swauth',
-    packages=find_packages(exclude=['test_swauth', 'bin']),
-    test_suite='nose.collector',
-    cmdclass=cmdclass,
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 2.6',
-        'Environment :: No Input/Output (Daemon)',
-        ],
-    install_requires=[],  # removed for better compat
-    scripts=[
-        'bin/swauth-add-account', 'bin/swauth-add-user',
-        'bin/swauth-cleanup-tokens', 'bin/swauth-delete-account',
-        'bin/swauth-delete-user', 'bin/swauth-list', 'bin/swauth-prep',
-        'bin/swauth-set-account-service',
-        ],
-    entry_points={
-        'paste.filter_factory': [
-            'swauth=swauth.middleware:filter_factory',
-            ],
-        },
-    )
+setuptools.setup(
+    setup_requires=['pbr'],
+    pbr=True)
