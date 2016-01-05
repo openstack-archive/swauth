da906b14a3f04bc858eb541361af86c2dac14f33
zhangguoqing <zhang.guoqing@99cloud.net>
2016-01-05 14:42:52 +0000

    Do not use __builtin__ in python3
    __builtin__ does not exist in Python 3, use
    six.moves.builtins instead.
    
    Change-Id: I1256f7df641af1085daca1d1ad2c34cb44cda265
    closes-bug: #1290234

--- test/__init__.py
+++ test/__init__.py
@@ -1,6 +1,6 @@
 # See http://code.google.com/p/python-nose/issues/detail?id=373
 # The code below enables nosetests to work with i18n _() blocks
 
-import __builtin__
+import six.moves.builtins as __builtin__
 
 setattr(__builtin__, '_', lambda x: x)
